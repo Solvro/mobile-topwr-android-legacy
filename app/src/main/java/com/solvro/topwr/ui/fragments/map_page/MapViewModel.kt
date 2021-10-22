50607ccfbfd0e3282f631046793d5ce1c1f06caa
Vango121 <barski93@gmail.com>
2021-10-22 13:17:34 +0200

    create fragments

---
+++ app/src/main/java/com/solvro/topwr/ui/fragments/map_page/MapViewModel.kt
@@ -0,0 +1,7 @@
+package com.solvro.topwr.ui.fragments.map_page
+
+import androidx.lifecycle.ViewModel
+
+class MapViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/solvro/topwr/ui/fragments/science_clubs_details/ScienceClubsDetailsViewModel.kt
@@ -0,0 +1,7 @@
+package com.solvro.topwr.ui.fragments.science_clubs_details
+
+import androidx.lifecycle.ViewModel
+
+class ScienceClubsDetailsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
