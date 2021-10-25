ae3f76adf02179959ef413a48728da242c9ce0a1
Marcel Barski <barski93@gmail.com>
2021-10-25 13:33:10 +0000

    Resolve "Create nav_graph, fragments"

---
+++ app/src/main/java/com/solvro/topwr/ui/fragments/science_clubs_page/ScienceClubsViewModel.kt
@@ -0,0 +1,7 @@
+package com.solvro.topwr.ui.fragments.science_clubs_page
+
+import androidx.lifecycle.ViewModel
+
+class ScienceClubsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
