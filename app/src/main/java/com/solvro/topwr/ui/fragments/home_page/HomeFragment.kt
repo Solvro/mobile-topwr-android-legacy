2bd067973235bf2b6ba01b87dd511f557cb51cbe
Vango121 <barski93@gmail.com>
2021-10-29 14:12:08 +0200

    set up HomeFragment.kt

--- app/src/main/java/com/solvro/topwr/ui/fragments/home_page/HomeFragment.kt
+++ app/src/main/java/com/solvro/topwr/ui/fragments/home_page/HomeFragment.kt
@@ -6,27 +6,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.solvro.topwr.R
+import com.solvro.topwr.databinding.HomeFragmentBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeFragment : Fragment() {
 
     companion object {
         fun newInstance() = HomeFragment()
     }
-
-    private lateinit var viewModel: HomeViewModel
+    private lateinit var binding: HomeFragmentBinding
+    private val viewModel: HomeViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.home_fragment, container, false)
+    ): View {
+        binding = HomeFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+
 
 }
