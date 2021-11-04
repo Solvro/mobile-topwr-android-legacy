46a4ff6e911e9ad6cb925f289e3c4281617ae066
RadoslawCzubak <radoslaw.marek.czubak@gmail.com>
2021-11-04 13:50:47 +0100

    remove unused import

--- app/src/main/java/com/solvro/topwr/ui/fragments/science_clubs_page/ScienceClubsAdapter.kt
+++ app/src/main/java/com/solvro/topwr/ui/fragments/science_clubs_page/ScienceClubsAdapter.kt
@@ -4,7 +4,6 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
 import com.solvro.topwr.data.model.scienceclub.ScienceClub
