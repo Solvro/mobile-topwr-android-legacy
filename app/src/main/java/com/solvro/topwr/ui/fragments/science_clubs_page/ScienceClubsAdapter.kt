259f42bcb099eca0f95fb9b9d7b5439c4b9f69ff
RadoslawCzubak <radoslaw.marek.czubak@gmail.com>
2021-11-04 13:41:31 +0100

    add rounded corners to imageview

--- app/src/main/java/com/solvro/topwr/ui/fragments/science_clubs_page/ScienceClubsAdapter.kt
+++ app/src/main/java/com/solvro/topwr/ui/fragments/science_clubs_page/ScienceClubsAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
 import com.solvro.topwr.data.model.scienceclub.ScienceClub
@@ -28,7 +29,8 @@ class ScienceClubsAdapter : RecyclerView.Adapter<ScienceClubsAdapter.ViewHolder>
                 Glide
                     .with(root.context)
                     .load("https://picsum.photos/200")
-                    .apply(RequestOptions.bitmapTransform(RoundedCorners(8)))
+                    .apply(RequestOptions().override(100, 100)) //change to 100x100px img
+                    .apply(RequestOptions.bitmapTransform(RoundedCorners(10)))
                     .into(scienceClubItemImage)
             }
         }
