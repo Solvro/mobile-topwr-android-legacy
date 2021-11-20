3fbaea9e0a7d87571bcab91f54a15d37b7897900
Vango121 <barski93@gmail.com>
2021-11-20 12:48:09 +0100

    rename whatsup to building

---
+++ app/src/main/java/com/solvro/topwr/ui/adapters/BuildingsAdapter.kt
@@ -0,0 +1,52 @@
+package com.solvro.topwr.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.solvro.topwr.R
+import com.solvro.topwr.data.model.maps.Maps
+import com.solvro.topwr.databinding.BuildingsItemBinding
+
+class BuildingsAdapter(
+    private val buildings: List<Maps>,
+    private val onClick: (Maps) -> Unit
+) : RecyclerView.Adapter<BuildingsAdapter.RecentlySearchedViewHolder>() {
+    inner class RecentlySearchedViewHolder(binding: BuildingsItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            //set OnCLickListener and send data with function
+            binding.root.setOnClickListener {
+                onClick(buildings[adapterPosition])
+            }
+        }
+
+        private val buildingItemImage = binding.buildingItemImage
+        private val buildingItemTextView = binding.buildingItemTextView
+        fun bind() {
+            val options: RequestOptions = RequestOptions()
+                .centerCrop()
+            buildingItemTextView.text = buildings[adapterPosition].Code
+            Glide.with(buildingItemImage)
+                .load(buildings[adapterPosition].Photo?.url).apply(options)
+                .into(buildingItemImage)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecentlySearchedViewHolder {
+        val binding = BuildingsItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return RecentlySearchedViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RecentlySearchedViewHolder, position: Int) {
+        holder.bind()
+    }
+
+    override fun getItemCount(): Int = buildings.size
+}
--- app/src/main/java/com/solvro/topwr/ui/fragments/home_page/HomeFragment.kt
+++ app/src/main/java/com/solvro/topwr/ui/fragments/home_page/HomeFragment.kt
@@ -1,7 +1,6 @@
 package com.solvro.topwr.ui.fragments.home_page
 
 import android.annotation.SuppressLint
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,11 +9,10 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.solvro.topwr.R
 import com.solvro.topwr.data.model.department.DepartmentItem
 import com.solvro.topwr.databinding.HomeFragmentBinding
 import com.solvro.topwr.ui.adapters.DepartmentsHomeAdapter
-import com.solvro.topwr.ui.adapters.RecentlySearchedAdapter
+import com.solvro.topwr.ui.adapters.BuildingsAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -37,7 +35,7 @@ class HomeFragment : Fragment() {
 
     @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding.recentlySearchRecyclerView.layoutManager =
+        binding.buildingsRecyclerView.layoutManager =
             LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
         binding.departmentsRecyclerView.layoutManager =
             LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
@@ -47,9 +45,9 @@ class HomeFragment : Fragment() {
             DepartmentItem("2", "Wydział lfgd", "W-3", ""),
             DepartmentItem("3", "Wydział Informatyki", "W-4", "")
         )
-        binding.recentlySearchRecyclerView.adapter =
-            RecentlySearchedAdapter(list) { chosenDepartment ->
-                Toast.makeText(context, chosenDepartment.code, Toast.LENGTH_SHORT).show()
+        binding.buildingsRecyclerView.adapter =
+            BuildingsAdapter(list) { chosenBuilding ->
+                Toast.makeText(context, chosenBuilding.code, Toast.LENGTH_SHORT).show()
             }
 
         //livedata for end date and bind data
