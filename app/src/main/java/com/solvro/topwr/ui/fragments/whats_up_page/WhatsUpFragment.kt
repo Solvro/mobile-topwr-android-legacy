fa3e61760a765385f3e2d99858499d368e8ef7a5
Wojciech Begierski <wojtek.begierski@tlen.pl>
2022-02-21 15:16:31 +0100

    add small changes and ideas

--- app/src/main/java/com/solvro/topwr/ui/fragments/whats_up_page/WhatsUpFragment.kt
+++ app/src/main/java/com/solvro/topwr/ui/fragments/whats_up_page/WhatsUpFragment.kt
@@ -7,18 +7,22 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import com.solvro.topwr.R
 import com.solvro.topwr.databinding.WhatsUpFragmentBinding
 
-class WhatsUpFragment : Fragment() {
+//@AndroidEntryPoint
+class WhatsUpFragment : Fragment(R.layout.whats_up_fragment) {
 
     companion object{
         fun newInstance() = WhatsUpFragment()
     }
 
-    //inject glide for photo loading?
+    //@Inject
+    //lateinit var glide: RequestManager
+
     private lateinit var binding: WhatsUpFragmentBinding
     private val viewModel: WhatsUpViewModel by viewModels()
-    //private val args: whats_up_fragmentArgs by navArgs()
+    //private val args: WhatsUpFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,6 +34,38 @@ class WhatsUpFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        /*
+        val notice = args.notice;
+
+        glide.load(notice.Photo?.url).into(binding.whatsUpImageView)
+
+        binding.apply {
+            whatsUpTitle.text = notice.Title
+            whatsUpNewsDescription.text = notice.Description
+            whatsUpDate.text = notice.published_at
+
+            backToMainBtn.setOnClickListener {
+                activity?.onBackPressed()
+            }
+        }
+        */
     }
+
+    /*
+       //example di for glide
+
+       @Singleton
+       @Provides
+       fun provideGlideInstance(
+           @ApplicationContext context: Context
+       ) = Glide.with(context).setDefaultRequestOptions(
+           RequestOptions()
+               .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+               //.placeholder()
+               //.error()
+               .priority(Priority.NORMAL)
+       )
+   */
 
 }
