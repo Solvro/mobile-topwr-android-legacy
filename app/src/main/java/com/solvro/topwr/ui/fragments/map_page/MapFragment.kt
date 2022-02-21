2747f40794469cf646612ee7730e381cb5c1e1d6
RadoslawCzubak <radoslaw.marek.czubak@gmail.com>
2022-02-21 23:29:04 +0100

    Add map to fragment.

--- app/src/main/java/com/solvro/topwr/ui/fragments/map_page/MapFragment.kt
+++ app/src/main/java/com/solvro/topwr/ui/fragments/map_page/MapFragment.kt
@@ -1,32 +1,44 @@
 package com.solvro.topwr.ui.fragments.map_page
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.google.android.gms.maps.MapsInitializer
+import com.google.android.gms.maps.SupportMapFragment
 import com.solvro.topwr.R
+import com.solvro.topwr.databinding.MapFragmentBinding
 
 class MapFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = MapFragment()
-    }
-
-    private lateinit var viewModel: MapViewModel
+    private val viewModel: MapViewModel by viewModels()
+    private lateinit var binding: MapFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.map_fragment, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.map_fragment, container, false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MapViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupMaps()
     }
 
+    private fun setupMaps() {
+        MapsInitializer.initialize(requireContext().applicationContext)
+        val mapFragment = childFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync {}
+    }
+
+    companion object {
+        fun newInstance() = MapFragment()
+    }
 }
--- app/src/main/java/com/solvro/topwr/ui/fragments/map_page/MapViewModel.kt
+++ app/src/main/java/com/solvro/topwr/ui/fragments/map_page/MapViewModel.kt
@@ -1,7 +1,10 @@
 package com.solvro.topwr.ui.fragments.map_page
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class MapViewModel : ViewModel() {
+@HiltViewModel
+class MapViewModel @Inject constructor() : ViewModel() {
     // TODO: Implement the ViewModel
 }
