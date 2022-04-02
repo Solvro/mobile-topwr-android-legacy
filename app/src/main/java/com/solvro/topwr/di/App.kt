5f46564d66d2e8305edccec2d1d3659d403b99aa
RadoslawCzubak <radoslaw.marek.czubak@gmail.com>
2022-04-02 16:38:26 +0200

    force light mode

--- app/src/main/java/com/solvro/topwr/di/App.kt
+++ app/src/main/java/com/solvro/topwr/di/App.kt
@@ -1,8 +1,15 @@
 package com.solvro.topwr.di
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        //Force Light Mode
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+    }
 }
