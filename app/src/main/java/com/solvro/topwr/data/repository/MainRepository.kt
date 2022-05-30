e229d7abec1851c010b50675727d9e4b32e1b338
RadoslawCzubak <radoslaw.marek.czubak@gmail.com>
2022-05-30 17:13:09 +0000

    Resolve "Campus Map View"

--- app/src/main/java/com/solvro/topwr/data/repository/MainRepository.kt
+++ app/src/main/java/com/solvro/topwr/data/repository/MainRepository.kt
@@ -2,18 +2,19 @@ package com.solvro.topwr.data.repository
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.liveData
+import com.solvro.topwr.data.local.DataStoreSource
 import com.solvro.topwr.data.model.departments.Departments
 import com.solvro.topwr.data.model.endDate.EndDate
 import com.solvro.topwr.data.model.endDate.WeekDayException
-import com.solvro.topwr.data.model.maps.Maps
+import com.solvro.topwr.data.model.maps.Building
 import com.solvro.topwr.data.model.notices.Notices
-import com.solvro.topwr.data.model.scienceClubs.ScienceClubs
 import com.solvro.topwr.data.remote.RemoteDataSource
 import com.solvro.topwr.utils.Resource
 import javax.inject.Inject
 
 class MainRepository @Inject constructor(
-    private val remoteDataSource: RemoteDataSource
+    private val remoteDataSource: RemoteDataSource,
+    private val dataStoreSource: DataStoreSource
 ) {
     //get Academic end of year date
     // @return LiveData<Resource<EndDate>> with End Date info
@@ -25,10 +26,13 @@ class MainRepository @Inject constructor(
     fun getDepartments(): LiveData<Resource<List<Departments>>> =
         liveData { emit(remoteDataSource.getDepartments()) }
 
-    fun getScienceClubs(): LiveData<Resource<List<ScienceClubs>>> =
-        liveData { emit(remoteDataSource.getScientificCircles()) }
+    fun getMaps(): LiveData<Resource<List<Building>>> =
+        liveData {
+            emit(Resource.loading(null))
+            emit(remoteDataSource.getMaps())
+        }
 
-    fun getMaps(): LiveData<Resource<List<Maps>>> = liveData { emit(remoteDataSource.getMaps()) }
+    fun getScienceClubs() = liveData { emit(remoteDataSource.getScientificCircles()) }
 
     fun getNotices(): LiveData<Resource<List<Notices>>> =
         liveData { emit(remoteDataSource.getNotices()) }
@@ -36,4 +40,10 @@ class MainRepository @Inject constructor(
     fun getWeekDayException(): LiveData<Resource<WeekDayException>> = liveData {
         emit(remoteDataSource.getWeekDayException())
     }
+
+    fun addIdToBuildingsSearchHistory(id: Int) {
+        dataStoreSource.addToBuildingsSearchHistory(id)
+    }
+
+    fun getBuildingsSearchHistory() = dataStoreSource.getBuildingsSearchHistory()
 }
--- app/src/main/java/com/solvro/topwr/ui/fragments/map_page/MapViewModel.kt
+++ app/src/main/java/com/solvro/topwr/ui/fragments/map_page/MapViewModel.kt
@@ -1,7 +1,37 @@
 package com.solvro.topwr.ui.fragments.map_page
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.solvro.topwr.data.model.maps.Building
+import com.solvro.topwr.data.repository.MainRepository
+import com.solvro.topwr.utils.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class MapViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class MapViewModel @Inject constructor(
+    private val repository: MainRepository
+) : ViewModel() {
+
+    private val _buildings = repository.getMaps()
+    val buildings: LiveData<Resource<List<Building>>> get() = _buildings
+
+    private val _selectedBuilding = MutableLiveData<Building?>(null)
+    val selectedBuilding: LiveData<Building?> get() = _selectedBuilding
+
+    private val _searchHistory = MutableLiveData(repository.getBuildingsSearchHistory())
+    val searchHistory: LiveData<List<Int>> get() = _searchHistory
+
+    var searchText = MutableLiveData("")
+
+    fun selectBuilding(building: Building) {
+        _selectedBuilding.postValue(if (building != selectedBuilding.value) building else null)
+        building.id?.let {
+            repository.addIdToBuildingsSearchHistory(it)
+        }
+
+        //refresh search history
+        _searchHistory.postValue(repository.getBuildingsSearchHistory())
+    }
 }
