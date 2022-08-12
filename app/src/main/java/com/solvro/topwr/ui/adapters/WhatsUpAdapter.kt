8c50f5e1d7469fb948699671a1dc4e69b5272ea7
Wojciech Begierski <wojtek.begierski@tlen.pl>
2022-08-12 16:58:05 +0200

    add shared element transition to whats up items

--- app/src/main/java/com/solvro/topwr/ui/adapters/WhatsUpAdapter.kt
+++ app/src/main/java/com/solvro/topwr/ui/adapters/WhatsUpAdapter.kt
@@ -2,26 +2,26 @@ package com.solvro.topwr.ui.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
-import com.bumptech.glide.load.resource.bitmap.CenterInside
 import com.bumptech.glide.load.resource.bitmap.GranularRoundedCorners
 import com.bumptech.glide.request.RequestOptions
-import com.solvro.topwr.R
 import com.solvro.topwr.data.model.notices.Notices
 import com.solvro.topwr.databinding.WhatsUpItemBinding
 
 class WhatsUpAdapter(
     private val notices: List<Notices>,
-    private val onClick: (Notices) -> Unit
+    private val onClick: (Notices, ImageView, TextView, TextView, TextView) -> Unit
 ) : RecyclerView.Adapter<WhatsUpAdapter.ViewHolder>() {
     inner class ViewHolder(binding: WhatsUpItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
         init {
             //set OnCLickListener and send data with function
             binding.moreButton.setOnClickListener {
-                onClick(notices[adapterPosition])
+                onClick(notices[adapterPosition], whatsupItemImage, whatsUpTitle, whatsUpDate, whatsUpDescription)
             }
         }
 
@@ -45,6 +45,11 @@ class WhatsUpAdapter(
             whatsUpDate.text = dateArray?.get(2)?.substring(0,2) + "." + (dateArray?.get(1)) + "." + (dateArray?.get(0))
             whatsUpTitle.text = notices[adapterPosition].title
             whatsUpDescription.text = notices[adapterPosition].description
+
+            whatsupItemImage.transitionName = notices[adapterPosition].photo!!.id.toString()
+            whatsUpTitle.transitionName = notices[adapterPosition].title.toString()
+            whatsUpDate.transitionName = notices[adapterPosition].created_at+notices[adapterPosition].id
+            whatsUpDescription.transitionName = notices[adapterPosition].description.toString()
         }
     }
 
--- app/src/main/java/com/solvro/topwr/ui/fragments/whats_up_page/WhatsUpFragment.kt
+++ app/src/main/java/com/solvro/topwr/ui/fragments/whats_up_page/WhatsUpFragment.kt
@@ -1,13 +1,20 @@
 package com.solvro.topwr.ui.fragments.whats_up_page
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.doOnPreDraw
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.transition.TransitionInflater
 import com.bumptech.glide.RequestManager
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.solvro.topwr.R
 import com.solvro.topwr.databinding.WhatsUpFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -26,11 +33,20 @@ class WhatsUpFragment : Fragment(R.layout.whats_up_fragment) {
     private lateinit var binding: WhatsUpFragmentBinding
     private val viewModel: WhatsUpViewModel by viewModels()
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        sharedElementEnterTransition = TransitionInflater.from(context!!)
+            .inflateTransition(R.transition.move)
+        sharedElementReturnTransition = TransitionInflater.from(context!!)
+            .inflateTransition(R.transition.move)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = WhatsUpFragmentBinding.inflate(inflater, container, false)
+
         return binding.root
     }
 
@@ -38,16 +54,51 @@ class WhatsUpFragment : Fragment(R.layout.whats_up_fragment) {
         super.onViewCreated(view, savedInstanceState)
 
         val notice = viewModel.notice.value
-        glide.load(notice?.photo?.url).into(binding.whatsUpImageView)
+
+        (view!!.parent as? ViewGroup)?.doOnPreDraw {
+            startPostponedEnterTransition()
+        }
 
         binding.apply {
+            whatsUpImageView.transitionName = notice!!.photo!!.id.toString()
+            whatsUpTitle.transitionName = notice.title.toString()
+            whatsUpDate.transitionName = notice.created_at+notice.id
+            whatsUpNewsDescription.transitionName = notice.description.toString()
+
             whatsUpTitle.text = notice?.title
             whatsUpNewsDescription.text = notice?.description
             whatsUpDate.text = notice?.getFormattedTime()
 
             backToMainBtn.setOnClickListener {
+                startPostponedEnterTransition()
                 findNavController().navigateUp()
             }
         }
+
+        glide.load(notice?.photo?.url)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    startPostponedEnterTransition()
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    startPostponedEnterTransition()
+                    return false
+                }
+
+            })
+            .into(binding.whatsUpImageView)
     }
 }
