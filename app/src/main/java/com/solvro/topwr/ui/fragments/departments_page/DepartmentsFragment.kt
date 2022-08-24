56121b806b605248ba43f7438eb081a4dc08b4fd
RadoslawCzubak <radoslaw.marek.czubak@gmail.com>
2022-08-24 14:57:01 +0200

    add api connection

--- app/src/main/java/com/solvro/topwr/ui/fragments/departments_page/DepartmentsFragment.kt
+++ app/src/main/java/com/solvro/topwr/ui/fragments/departments_page/DepartmentsFragment.kt
@@ -1,15 +1,16 @@
 package com.solvro.topwr.ui.fragments.departments_page
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.solvro.topwr.databinding.DepartmentsFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class DepartmentsFragment : Fragment() {
@@ -20,12 +21,12 @@ class DepartmentsFragment : Fragment() {
 
     private lateinit var binding: DepartmentsFragmentBinding
     private val viewModel: DepartmentsViewModel by viewModels()
-    private lateinit var adapter: DepartmentsAdapter
+    private var departmentsAdapter = DepartmentsAdapter(DepartmentComparator)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = DepartmentsFragmentBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -33,13 +34,21 @@ class DepartmentsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupRecyclerView()
+        setupObservers()
     }
 
     private fun setupRecyclerView() {
-        adapter = DepartmentsAdapter()
-        binding.departmentRecyclerView.adapter = adapter
-        binding.departmentRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.departmentRecyclerView.apply {
+            adapter = departmentsAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
     }
 
-
+    private fun setupObservers() {
+        with(viewModel) {
+            departments.observe(viewLifecycleOwner) {
+                lifecycleScope.launch { departmentsAdapter.submitData(it) }
+            }
+        }
+    }
 }
--- app/src/main/java/com/solvro/topwr/ui/fragments/departments_page/DepartmentsViewModel.kt
+++ app/src/main/java/com/solvro/topwr/ui/fragments/departments_page/DepartmentsViewModel.kt
@@ -1,10 +1,39 @@
 package com.solvro.topwr.ui.fragments.departments_page
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.solvro.topwr.data.model.departments.Departments
+import com.solvro.topwr.data.repository.MainRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.cancellable
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class DepartmentsViewModel @Inject constructor() : ViewModel() {
-    // TODO: Implement the ViewModel
+class DepartmentsViewModel @Inject constructor(
+    private val repository: MainRepository
+) : ViewModel() {
+
+    private val _departments by lazy { MutableLiveData<PagingData<Departments>>() }
+    val departments: LiveData<PagingData<Departments>> by lazy { _departments }
+
+    init {
+        getDepartments()
+    }
+
+    private fun getDepartments() {
+        viewModelScope.launch {
+            repository.getDepartmentsPaged()
+                .cancellable()
+                .cachedIn(viewModelScope)
+                .collectLatest {
+                    _departments.postValue(it)
+                }
+        }
+    }
 }
