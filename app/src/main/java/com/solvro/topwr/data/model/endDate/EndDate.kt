5cd3b619246f547339c1fa3e647ffe608a583b2e
RadoslawCzubak <radoslaw.marek.czubak@gmail.com>
2022-09-01 09:16:34 +0000

    Resolve "FIX: Week-day title"

--- app/src/main/java/com/solvro/topwr/data/model/endDate/EndDate.kt
+++ app/src/main/java/com/solvro/topwr/data/model/endDate/EndDate.kt
@@ -2,7 +2,7 @@ package com.solvro.topwr.data.model.endDate
 
 data class EndDate(
     val id: Int?,
-    val EndDate: String?,
+    val endDate: String?,
     val published_at: String?,
     val created_at: String?,
     val updated_at: String?
--- app/src/main/java/com/solvro/topwr/ui/fragments/home_page/HomeViewModel.kt
+++ app/src/main/java/com/solvro/topwr/ui/fragments/home_page/HomeViewModel.kt
@@ -1,21 +1,26 @@
 package com.solvro.topwr.ui.fragments.home_page
 
-import android.util.Log
 import androidx.lifecycle.*
 import com.solvro.topwr.data.model.date.Date
+import com.solvro.topwr.data.model.endDate.EndDate
+import com.solvro.topwr.data.model.endDate.Weekday
 import com.solvro.topwr.data.model.scienceClub.ScienceClub
 import com.solvro.topwr.data.repository.MainRepository
+import com.solvro.topwr.utils.AcademicDayMapper
+import com.solvro.topwr.utils.Constants
 import com.solvro.topwr.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
-import java.text.SimpleDateFormat
-import java.util.*
-import java.util.concurrent.TimeUnit
+import org.threeten.bp.LocalDate
+import org.threeten.bp.format.DateTimeFormatter
+import org.threeten.bp.temporal.ChronoField
+import org.threeten.bp.temporal.ChronoUnit
 import javax.inject.Inject
-import kotlin.math.abs
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(private val repository: MainRepository) : ViewModel() {
+
+    /* LiveData */
     val departments = repository.getDepartments()
     val buildings = repository.getMaps()
     val notices = repository.getNotices()
@@ -28,55 +33,43 @@ class HomeViewModel @Inject constructor(private val repository: MainRepository) 
 
     private val _endDate = repository.getEndDate()
     val endDate: LiveData<String> = Transformations.switchMap(_endDate) { dateObj ->
-        var daysString = ""
-        if (dateObj.data?.EndDate != null) {
-            val date = SimpleDateFormat("yyyy-MM-dd", Locale.ROOT).parse(dateObj.data.EndDate)
-            val curr_date = Calendar.getInstance().time
-            val diff = abs(date.time - curr_date.time)
-            val days = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS)
-            daysString += if (days < 100) {
-                "0" + days.toString()
-            } else {
-                days.toString()
-            }
+        var daysString = "0"
+        if (dateObj.data?.endDate != null) {
+            daysString = (calculateDaysToEndDate(dateObj.data) ?: "0")
+                .padStart(3, '0')
         }
         liveData<String> { emit(daysString) }
     }
 
     private val _dateWeek = repository.getWeekDayException()
     val dateWeek: LiveData<Date> = Transformations.switchMap(_dateWeek) { exceptionObj ->
-        var date = Date(0, false)
-        if (exceptionObj.data != null && exceptionObj.data.Weekday != null) {
-            for (weekDay in exceptionObj.data.Weekday) {
-                val curr_date = SimpleDateFormat("yyyy-MM-dd", Locale.ROOT).format(java.util.Date())
-                if (weekDay.Date.equals(curr_date)) {
-                    var dayOfWeek = 0
-                    when (weekDay.DayOfTheWeek) {
-                        "Mon" -> dayOfWeek = Calendar.MONDAY
-                        "Tue" -> dayOfWeek = Calendar.TUESDAY
-                        "Wed" -> dayOfWeek = Calendar.WEDNESDAY
-                        "Thu" -> dayOfWeek = Calendar.THURSDAY
-                        "Fri" -> dayOfWeek = Calendar.FRIDAY
-                    }
-                    var parity: Boolean = true
-                    when (weekDay.Parity) {
-                        "Odd" -> parity = false
-                        "Even" -> parity = true
-                    }
-                    date = Date(dayOfWeek, parity)
-                } else {
-                    val calendar = Calendar.getInstance()
-                    val day = calendar[Calendar.DAY_OF_WEEK]
-                    val week = calendar.get(Calendar.WEEK_OF_YEAR) + 1
-                    Log.i("Week", week.toString())
-                    date = Date(day, week % 2 == 0)
-
-                }
-            }
+        val dateFormatter = DateTimeFormatter.ofPattern(Constants.DEFAULT_DATE_PATTERN)
+        val dayException = exceptionObj.data?.weekday?.find {
+            val exceptionDate = LocalDate.parse(it.date, dateFormatter)
+            val currDate = LocalDate.now()
+            exceptionDate.equals(currDate)
         }
+        val date = getAcademicDate(dayException)
         liveData { emit(date) }
-
     }
+
+    private fun getAcademicDate(
+        dayException: Weekday?
+    ): Date {
+        return if (dayException != null) {
+            val dayOfWeek =
+                AcademicDayMapper.mapWeekDayStringToCalendarWeekDay(dayException.dayOfTheWeek ?: "")
+            val parity = AcademicDayMapper.mapParityToBoolean(dayException.parity ?: "")
+            Date(dayOfWeek, parity)
+        } else {
+            val currDate = LocalDate.now()
+            Date(
+                currDate.dayOfWeek.value + 1,
+                currDate.get(ChronoField.ALIGNED_WEEK_OF_YEAR) % 2 == 0
+            )
+        }
+    }
+
 
     private fun getScienceClubs(scienceClubsLiveData: MutableLiveData<Resource<List<ScienceClub>>>) {
         scienceClubsLiveData.value = Resource(
@@ -90,5 +83,13 @@ class HomeViewModel @Inject constructor(private val repository: MainRepository) 
                 scienceClubsLiveData.postValue(response)
             }
         }
+    }
+
+    private fun calculateDaysToEndDate(data: EndDate): String? {
+        val dateFormatter = DateTimeFormatter.ofPattern(Constants.DEFAULT_DATE_PATTERN)
+        val endDate = LocalDate.parse(data.endDate, dateFormatter).minusDays(1)
+        val currDate = LocalDate.now()
+        val daysDiff = ChronoUnit.DAYS.between(currDate, endDate)
+        return if (daysDiff < 0) null else daysDiff.toString()
     }
 }
