42adec4881b55306e1695b116d4568e9050a41d0
RadoslawCzubak <radoslaw.marek.czubak@gmail.com>
2022-09-12 19:10:02 +0200

    fix departments order

--- app/src/main/java/com/solvro/topwr/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/solvro/topwr/data/remote/RemoteDataSource.kt
@@ -27,7 +27,7 @@ class RemoteDataSource @Inject constructor(
 
     //get list of departments
     suspend fun getDepartments(
-        startIndex: Int = 1,
+        startIndex: Int = 0,
         limit: Int = 20
     ): Resource<List<Departments>> =
         getResult {
