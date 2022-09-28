edd13a2378b5731d1bc0ae8bf4fa0cfae187cb33
RadoslawCzubak <radoslaw.marek.czubak@gmail.com>
2022-09-28 08:05:57 +0000

    Resolve "Pre-release fixes 3"

--- app/src/main/java/com/solvro/topwr/data/model/departments/Icon.kt
+++ app/src/main/java/com/solvro/topwr/data/model/departments/Icon.kt
@@ -1,24 +1,27 @@
 package com.solvro.topwr.data.model.departments
 
+import android.os.Parcelable
 import com.solvro.topwr.data.model.common.ProviderMetadata
-import java.io.Serializable
+import kotlinx.parcelize.Parcelize
+import kotlinx.parcelize.RawValue
 
+@Parcelize
 data class Icon(
     val alternativeText: String?,
     val caption: String?,
     val created_at: String?,
     val ext: String?,
-    val formats: Any?,
+    val formats: @RawValue Any?,
     val hash: String?,
     val height: Int?,
     val id: Int?,
     val mime: String?,
     val name: String?,
-    val previewUrl: Any?,
+    val previewUrl: @RawValue Any?,
     val provider: String?,
     val provider_metadata: ProviderMetadata?,
     val size: Double?,
     val updated_at: String?,
     val url: String?,
     val width: Int?
-) : Serializable
+) : Parcelable
