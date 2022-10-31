d5f14b7d2cf743e3b3001d62c450b7bf31925a16
RadoslawCzubak <radoslaw.marek.czubak@gmail.com>
2022-10-31 11:24:21 +0100

    add GetDepartmentUseCase

--- app/src/main/java/com/solvro/topwr/features/scienceclub/presentation/details/ScienceClubsDetailsViewModel.kt
+++ app/src/main/java/com/solvro/topwr/features/scienceclub/presentation/details/ScienceClubsDetailsViewModel.kt
@@ -1,22 +1,21 @@
 package com.solvro.topwr.features.scienceclub.presentation.details
 
 import androidx.lifecycle.*
-import com.solvro.topwr.data.repository.MainRepository
 import com.solvro.topwr.features.scienceclub.domain.model.ScienceClub
+import com.solvro.topwr.features.scienceclub.domain.usecase.GetScienceClubDepartmentUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ScienceClubsDetailsViewModel @Inject constructor(
-    private val mainRepository: MainRepository,
+    private val getScienceClubDepartmentUseCase: GetScienceClubDepartmentUseCase,
     savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
     private val _scienceClub by lazy { MutableLiveData<ScienceClub>() }
     val scienceClub: LiveData<ScienceClub> by lazy { _scienceClub }
 
-    private val _departmentName by lazy { MutableLiveData<String?>() }
+    private val _departmentName by lazy { MutableLiveData<String?>(null) }
     val departmentName: LiveData<String?> by lazy { _departmentName }
 
     init {
@@ -26,13 +25,10 @@ class ScienceClubsDetailsViewModel @Inject constructor(
     }
 
     private fun getScienceClubDepartment(departmentNumber: Int?) {
-        if (departmentNumber == null) {
-            _departmentName.postValue(null)
-            return
-        }
-        viewModelScope.launch {
-            val department = mainRepository.getDepartment(departmentNumber)
-            _departmentName.postValue(department.data?.name ?: "")
+        departmentNumber?.let {
+            getScienceClubDepartmentUseCase(departmentNumber, viewModelScope) {
+                _departmentName.postValue(it.data?.name)
+            }
         }
     }
 }
