96879eb7d393e6d65c98009a2bf2b3142f2046bd
Wojciech Begierski <wojtek.begierski@tlen.pl>
2022-11-05 00:51:34 +0100

    refactor usecases

--- app/src/main/java/com/solvro/topwr/features/departments/domain/use_case/GetDepartmentsUseCase.kt
+++ app/src/main/java/com/solvro/topwr/features/departments/domain/use_case/GetDepartmentsUseCase.kt
@@ -1,18 +1,39 @@
 package com.solvro.topwr.features.departments.domain.use_case
 
 import androidx.paging.PagingData
+import androidx.paging.filter
 import com.solvro.topwr.core.api.model.departments.DepartmentsRemote
 import com.solvro.topwr.core.base.FlowUseCase
 import com.solvro.topwr.features.departments.domain.DepartmentsRepository
 import com.solvro.topwr.features.departments.domain.model.Departments
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.cancellable
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.transform
 import javax.inject.Inject
 
 class GetDepartmentsUseCase @Inject constructor(
     private val departmentsRepository: DepartmentsRepository
-) : FlowUseCase<Unit, PagingData<Departments>>() {
+) : FlowUseCase<GetDepartmentsParams, PagingData<Departments>>() {
 
-    override suspend fun action(params: Unit): Flow<PagingData<Departments>> {
+    private var getDepartmentsJob: Job? = null
+
+    override suspend fun action(params: GetDepartmentsParams): Flow<PagingData<Departments>> {
+        getDepartmentsJob?.cancel()
         return departmentsRepository.getDepartmentsPaged()
+            .cancellable()
+            .transform {
+                val filteredData = it.filter { department ->
+                    if (params.textFilter.lowercase() == "")    true
+                    else (department.name?.contains(params.textFilter.lowercase()) ?: false)
+                            || department.code?.lowercase()?.contains(params.textFilter.lowercase()) ?: false
+                }
+                emit(filteredData)
+            }
     }
 }
+
+data class GetDepartmentsParams(
+    val textFilter: String
+)
--- app/src/main/java/com/solvro/topwr/features/departments/domain/use_case/GetScienceClubsUseCase.kt
+++ app/src/main/java/com/solvro/topwr/features/departments/domain/use_case/GetScienceClubsUseCase.kt
@@ -1,17 +1,36 @@
 package com.solvro.topwr.features.departments.domain.use_case
 
 import androidx.paging.PagingData
+import androidx.paging.filter
 import com.solvro.topwr.core.base.FlowUseCase
 import com.solvro.topwr.data.model.scienceClub.ScienceClub
 import com.solvro.topwr.features.departments.domain.DepartmentsRepository
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.cancellable
+import kotlinx.coroutines.flow.transform
 import javax.inject.Inject
 
 class GetScienceClubsUseCase @Inject constructor(
     private val departmentsRepository: DepartmentsRepository
-) : FlowUseCase<Unit, PagingData<ScienceClub>>() {
+) : FlowUseCase<GetScienceClubsDetailsParams, PagingData<ScienceClub>>() {
 
-    override suspend fun action(params: Unit): Flow<PagingData<ScienceClub>> {
+    private var getScienceClubsJob: Job? = null
+
+    override suspend fun action(params: GetScienceClubsDetailsParams): Flow<PagingData<ScienceClub>> {
+        getScienceClubsJob?.cancel()
         return departmentsRepository.getScienceClubsPaged()
+            .cancellable()
+            .transform {
+                val filteredData = it.filter { scienceClub ->
+                    (scienceClub.department == params.displayOrder)
+                }
+
+                emit(filteredData)
+            }
     }
 }
+
+data class GetScienceClubsDetailsParams(
+    val displayOrder: Int?
+)
